11f88592856cc1b2589dff541d9a17608172af3e
dol <carrynong2@gmail.com>
2023-03-20 16:37:27 +0700

    test

---
+++ MyProject.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package MyProject;
+
+/**
+ *
+ * @author dol
+ */
+public class MyProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");        
+        System.out.println("Welcome to to club!!!");
+    }
+}
